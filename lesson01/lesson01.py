099087adc9f9c06188bcc0e48c66328081baa4e5
SoullessSoldier <gevork_ssh@mail.ru>
2023-09-30 12:21:34 +0200

    lesson 1 step 5 - looping for asynchronous function

--- lesson01/lesson01.py
+++ lesson01/lesson01.py
@@ -23,10 +23,10 @@ def draw(canvas):
     curses.curs_set(False)
     canvas.border(0, 0, 0, 0, 0, 0, 0, 0)
     coroutine = blink(canvas, row, column)
-    print('type', type(coroutine))
-    print('dir', dir(coroutine))
-    time.sleep(5)
-    # while True:
+    # print('type', type(coroutine))
+    # print('dir', dir(coroutine))
+    # time.sleep(5)
+    while True:
         # canvas.addstr(row, column, '*', curses.A_DIM)
         # canvas.refresh()
         # time.sleep(2)
@@ -39,15 +39,16 @@ def draw(canvas):
         # canvas.addstr(row, column, '*')
         # canvas.refresh()
         # time.sleep(0.3)
-    try:
-        coroutine.send(None)
-        canvas.refresh()
-    except StopIteration:
-        print('stop iteration')
+        try:
+            coroutine.send(None)
+            canvas.refresh()
+            time.sleep(1)
+        except StopIteration:
+            print('stop iteration')
     # break
 
 
-    time.sleep(5)
+    # time.sleep(5)
 
 
 if __name__ == '__main__':
