00ec056348e176e2df7cdef1e052eabdfcc59672
SoullessSoldier <gevork_ssh@mail.ru>
2023-09-30 12:18:33 +0200

    lesson 1 step 4 - add asynchronous function for blinking star

--- lesson01/lesson01.py
+++ lesson01/lesson01.py
@@ -1,24 +1,53 @@
 import curses
 import time
+import asyncio
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
     row, column = (3, 10)
     curses.curs_set(False)
     canvas.border(0, 0, 0, 0, 0, 0, 0, 0)
-    while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
+    coroutine = blink(canvas, row, column)
+    print('type', type(coroutine))
+    print('dir', dir(coroutine))
+    time.sleep(5)
+    # while True:
+        # canvas.addstr(row, column, '*', curses.A_DIM)
+        # canvas.refresh()
+        # time.sleep(2)
+        # canvas.addstr(row, column, '*')
+        # canvas.refresh()
+        # time.sleep(0.3)
+        # canvas.addstr(row, column, '*', curses.A_BOLD)
+        # canvas.refresh()
+        # time.sleep(0.5)
+        # canvas.addstr(row, column, '*')
+        # canvas.refresh()
+        # time.sleep(0.3)
+    try:
+        coroutine.send(None)
         canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+    except StopIteration:
+        print('stop iteration')
+    # break
+
+
+    time.sleep(5)
 
 
 if __name__ == '__main__':
